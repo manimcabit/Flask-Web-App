61ddf71843c91e901138dba7ce3d8aeb3c31dcf8
root <root@ip-172-31-6-237.us-east-2.compute.internal>
2023-03-07 15:40:08 +0000

    updated the port and local ip address

---
+++ main.py
@@ -0,0 +1,9 @@
+#from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True,port=8000)
